33c8be3d3081570097116c6cafa9d211c1a57dbe
Valentin Harquel <valentin@mac.home>
2025-12-27 18:54:21 +0100

    All codes

---
+++ fonts.py
@@ -0,0 +1,8 @@
+#Libraries
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+#Fonts
+font_path = "Montserrat-Regular.ttf"
+montserrat_font = fm.FontProperties(fname=font_path)
+
---
+++ test.py
@@ -0,0 +1,9 @@
+def addition (x, y) : 
+    return x + y 
+
+resultat = 1
+for i in range(1, 101, 1) : 
+    resultat = 1 + i
+    print(resultat)
+
+print(resultat)
